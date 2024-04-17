c6f849647481f8364694c326ba8628893932ba6e
Mike Smith <mango@live.cn>
2024-04-18 02:35:37 +0800

    concat texture incorrect

--- src/textures/concat.cpp
+++ src/textures/concat.cpp
@@ -111,6 +111,7 @@ public:
     [[nodiscard]] Float4 evaluate(const Interaction &it,
                                   const SampledWavelengths &swl,
                                   Expr<float> time) const noexcept override {
+        // TODO: incorrect implementation
         auto s = def(make_float4(0.f));
         auto index = 0u;
         for (auto i = 0u; i < _sub_textures.size() - 1; i++) {
