eec954d59b7ced8c1c9fd571b5fbb229423ddd84
omid ghotbi (TAO) officially Seyedhosein Ghotbidezuli <omidt.gh@gmail.com>
2025-05-13 18:49:45 +0200

    Update mega_volume_path.cpp

--- src/integrators/mega_volume_path.cpp
+++ src/integrators/mega_volume_path.cpp
@@ -1,3 +1,11 @@
 //
 // Created by Omid Ghotbi (TAO) base on mega-vpt by ChenXin.
 //
+
+#include <util/sampling.h>
+#include <base/pipeline.h>
+#include <base/integrator.h>
+#include <util/medium_tracker.h>
+#include <base/medium.h>
+#include <base/phase_function.h>
+#include <util/rng.h>
